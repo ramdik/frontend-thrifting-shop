f897d36da171b374161d7cf84426a562a8fc7146
Dhika <udinjogres13@gmail.com>
2024-02-05 19:34:53 +0700

    structure folder update

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,11 @@
+import './Login.css'
+
+const Login = () => {
+    return (
+        <div className='bg-warning container'>
+                <h1> Login Page</h1>
+        </div>
+    );
+};
+
+export default Login;
